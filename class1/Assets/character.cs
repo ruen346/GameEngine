7f807e3f9754fe674b66a397c78a9f5fcdfbd723
Ruen_note <ruen346@gmail.com>
2019-03-12 19:37:29 +0900

    3/12

---
+++ class1/Assets/character.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class character : MonoBehaviour {
+
+    int i = 0;
+    public int tset = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        print("NewBe.Update()" + i++);
+	}
+}
