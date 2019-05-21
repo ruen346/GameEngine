9d591c39fb2eddaeb4a8cceecc61aad6ae42535b
ruen_mainpc <ruen346@gmail.com>
2019-05-22 03:03:04 +0900

    적이동및 공격 끝

--- Homework1/Assets/Making/Shoot_enemy.cs
+++ Homework1/Assets/Making/Shoot_enemy.cs
@@ -18,14 +18,13 @@ namespace Complete
         public float m_MaxLaunchForce = 30f;        // The force given to the shell if the fire button is held for the max charge time.
         public float m_MaxChargeTime = 0.75f;       // How long the shell can charge for before it is fired at max force.
  
-        private float m_CurrentLaunchForce;         // The force that will be given to the shell when the fire button is released.
+        private float m_CurrentLaunchForce;         // 발사강도
         private float m_ChargeSpeed;                // How fast the launch force increases, based on the max charge time.
         private bool m_Fired;                       // Whether or not the shell has been launched with this button press.
 
 
         private void OnEnable()
         {
-            // When the tank is turned on, reset the launch force and the UI
             m_CurrentLaunchForce = m_MinLaunchForce;
             m_AimSlider.value = m_MinLaunchForce;
         }
